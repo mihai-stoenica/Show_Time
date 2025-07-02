ac2c4e3dc4b3dc711c99ce6218a54e2eca8c5575
mihai-stoenica <stoenica.mihai2016@gmail.com>
2025-07-02 14:42:28 +0300

    CRUD for festivals

---
+++ src/Controller/FestivalController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Festival;
+use App\Form\FestivalType;
+use App\Repository\FestivalRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/festival')]
+final class FestivalController extends AbstractController
+{
+    #[Route(name: 'app_festival_index', methods: ['GET'])]
+    public function index(FestivalRepository $festivalRepository): Response
+    {
+        return $this->render('festival/index.html.twig', [
+            'festivals' => $festivalRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_festival_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $festival = new Festival();
+        $form = $this->createForm(FestivalType::class, $festival);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($festival);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_festival_index');
+        }
+
+        return $this->render('festival/new.html.twig', [
+            'band' => $festival,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_festival_show', methods: ['GET'])]
+    public function show(Festival $festival): Response
+    {
+        return $this->render('festival/show.html.twig', [
+            'festival' => $festival,
+            'bands' => $festival->getBands(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_festival_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Festival $festival, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(FestivalType::class, $festival);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_festival_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('festival/edit.html.twig', [
+            'festival' => $festival,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_festival_delete', methods: ['POST'])]
+    public function delete(Request $request, Festival $festival, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $festival->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($festival);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_festival_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/FestivalType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Band;
+use App\Entity\Festival;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FestivalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('location')
+            ->add('start_date')
+            ->add('end_date')
+            ->add('bands', EntityType::class, [
+                'class' => Band::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Festival::class,
+        ]);
+    }
+}
